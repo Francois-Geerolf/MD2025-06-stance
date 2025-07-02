354fef724f74a1576f3e25e0466dd9d0d468d539
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-07-02 12:54:37 +0200

    update

--- figure6.R
+++ figure6.R
@@ -1,112 +1,69 @@
 library("tidyverse")
 library("scales")
 library("data.table")
+library("fredr")
+source("~/iCloud/passwords/keys.R")
 
-## Load ECB datasets ------
+## Load Fred datasets ------
 
-datasets_ECB <- c("FM")
+# Add passwords file: fred_key <- "Your FRED Key"
+fredr_set_key(fred_key)
 
-for (dataset in datasets_ECB){
-  download.file(paste0("https://sdw-wsrest.ecb.europa.eu/service/data/", dataset, "?format=csvdata"),
-                destfile = paste0(dataset, ".csv"),
-                quiet = F)
-  assign(dataset, fread(paste0(dataset, ".csv")))
-  unlink(paste0(dataset, ".csv"))
-}
+money <- map_dfr(c("WALCL", "ECBASSETSW"), fredr)
+
 
 ## English ------
 
-dates_ecb <- FM %>%
-  filter(PROVIDER_FM_ID == "DFR") %>%
-  mutate(date = as.Date(TIME_PERIOD)) %>%
-  select(-TIME_PERIOD) %>%
-  mutate(OBS_VALUE = OBS_VALUE-lag(OBS_VALUE)) %>%
-  filter(OBS_VALUE != 0) %>%
-  pull(date)
-
-figure5 <- FM %>%
-  filter(PROVIDER_FM_ID %in% c("DFR", "MLFR", "MRR_RT"),
-         DATA_TYPE_FM == "LEV",
-         FREQ == "D") %>%
-  mutate(date = as.Date(TIME_PERIOD)) %>%
-  select(-TIME_PERIOD) %>%
-  filter(date >= as.Date("2022-01-01")) %>%
-  #add_row(date = as.Date("2025-06-11"), PROVIDER_FM_ID = "DFR", OBS_VALUE = 2) %>%
-  #add_row(date = as.Date("2025-06-11"), PROVIDER_FM_ID = "MRR_RT", OBS_VALUE = 2.15) %>%
-  #add_row(date = as.Date("2025-06-11"), PROVIDER_FM_ID = "MLFR", OBS_VALUE = 2.4) %>%
-  #add_row(date = as.Date("2025-06-10"), PROVIDER_FM_ID = "DFR", OBS_VALUE = 2.25) %>%
-  #add_row(date = as.Date("2025-06-10"), PROVIDER_FM_ID = "MRR_RT", OBS_VALUE = 2.4) %>%
-  #add_row(date = as.Date("2025-06-10"), PROVIDER_FM_ID = "MLFR", OBS_VALUE = 2.65) %>%
-  arrange(desc(date)) %>%
-  #add_row(date = as.Date("2024-09-17"), PROVIDER_FM_ID = "DFR", OBS_VALUE = 3.5) %>%
-  #add_row(date = as.Date("2024-09-17"), PROVIDER_FM_ID = "MRR_RT", OBS_VALUE = 3.65) %>%
-  #add_row(date = as.Date("2024-09-17"), PROVIDER_FM_ID = "MLFR", OBS_VALUE = 3.9) %>%
-  left_join(PROVIDER_FM_ID,  by = "PROVIDER_FM_ID") %>%
-  mutate(Provider_fm_id = factor(PROVIDER_FM_ID,
-                                 levels = c("MLFR", "MRR_RT", "DFR"),
-                                 labels = c("ECB Marginal lending facility",
-                                            "ECB Main refinancing operations - Minimum bid rate/fixed rate",
-                                            "ECB Deposit facility")))
+figure6 <- money %>%
+  mutate(value = value/1000) %>%
+  mutate(Variable = factor(series_id,
+                           levels = c("WALCL", "ECBASSETSW"),
+                           labels = c("Federal Reserve", "European Central Bank"))) %>%
+  select(date, value, Variable) %>%
+  filter(date >= as.Date("2003-01-01"))
 
 
-ggplot(data = figure5) + geom_line(aes(x = date, y = OBS_VALUE / 100, color = Provider_fm_id)) + 
-  theme_minimal() + xlab("") + ylab("Interest Rates (%)") +
-  scale_x_date(breaks = c(dates_ecb),
-               labels = date_format("%d %B %Y")) +
-  scale_y_continuous(breaks = 0.01*seq(-10, 50, 0.5),
-                     labels = percent_format(accuracy = .1)) +
-  theme(legend.position = c(0.65, 0.2),
-        legend.title = element_blank(),
-        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-  geom_label_repel(data = . %>% filter(date == max(date)),
-                   aes(x = date, y = OBS_VALUE / 100, label = percent(OBS_VALUE / 100, acc = 0.01), color = Provider_fm_id))
+ggplot(data = figure6) + geom_line(aes(x = date, y = value, color = Variable)) +
+  xlab("") + ylab("Euros (ECB) or Dollars (Fed)") + theme_minimal() +
+  scale_color_manual(values = c("#003399", "#B22234")) +
+  scale_x_date(breaks = as.Date(paste0(seq(2003, 2100, 2), "-01-01")),
+               labels = date_format("%Y")) +
+  theme(legend.position = c(0.4, 0.9),
+        legend.title = element_blank()) +
+  scale_y_continuous(breaks = 1000*seq(-100, 90, .5),
+                     labels = scales::dollar_format(acc = 1, su = " Bn", pre = ""),
+                     limits = c(0, 9000))
 
 
-write_csv(figure5, "figure5.csv")
-ggsave("figure5.png", width = 1.25*6, height = 1.25*3.375, bg = "white")
-ggsave("figure5.pdf", width = 1.25*6, height = 1.25*3.375)
+write_csv(figure6, "figure6.csv")
+ggsave("figure6.png", width = 1.25*6, height = 1.25*3.375, bg = "white")
+ggsave("figure6.pdf", width = 1.25*6, height = 1.25*3.375)
 
 
 ## Français ------
 
-figure5_fr <- prc_hicp_midx |>
-  filter(coicop %in% c("TOT_X_NRG_FOOD", "CP00"),
-         unit == "I15") |>
-  mutate(year = substr(time, 1, 4)) |>
-  #filter(!(Geo %in% intersect(eurozone_countries, CEEC_countries))) |>
-  left_join(prc_hicp_cow |>
-              filter(statinfo == "COWEA20", geo != "EA20") |>
-              transmute(geo, year = time, country_weights = values, uniform_weights = 1),
-            by = c("geo", "year")) |>
-  mutate(date = as.Date(paste0(time, "-01"))) |>
-  filter(date >= as.Date("2000-01-01")) |>
-  group_by(geo, coicop) |>
-  arrange(date) |>
-  mutate(`Inflation sur 3 ans` = (values/lag(values, 36))^(1/3)-1,
-         `Inflation sur 1 an` = values/lag(values, 12)-1) |>
-  ungroup() |>
-  gather(variable, values, `Inflation sur 3 ans`, `Inflation sur 1 an`) |>
-  filter(!is.na(values)) |>
-  group_by(coicop, date, variable) |>
-  summarise(values = sqrt(Hmisc::wtd.var(values, country_weights))) |>
-  mutate(coicop = factor(coicop, 
-                       levels = c("TOT_X_NRG_FOOD", "CP00"),
-                       labels = c("Inflation sous-jacente", "Inflation globale")))
+figure6_fr <- money %>%
+  mutate(value = value/1000) %>%
+  mutate(Variable = factor(series_id,
+                           levels = c("WALCL", "ECBASSETSW"),
+                           labels = c("Réserve Fédérale", "Banque Centrale Européenne"))) %>%
+  select(date, value, Variable) %>%
+  filter(date >= as.Date("2003-01-01"))
 
-ggplot(data = figure5_fr) + geom_line(aes(x = date, y = values, color = coicop, linetype = coicop)) + 
-  theme_minimal() + xlab("") + ylab("Écart-type de l'inflation annuelle\nPondérée, Zone Euro (20 pays) (%)") +
-  scale_x_date(breaks = as.Date(paste0(seq(1960, 2100, 2), "-01-01")),
+
+ggplot(data = figure6_fr) + geom_line(aes(x = date, y = value, color = Variable)) +
+  xlab("") + ylab("Euros (ECB) ou Dollars (Fed)") + theme_minimal() +
+  scale_color_manual(values = c("#003399", "#B22234")) +
+  scale_x_date(breaks = as.Date(paste0(seq(2003, 2100, 2), "-01-01")),
                labels = date_format("%Y")) +
-  scale_color_manual(values = c("#1E1C1C", "#A81630")) + 
-  scale_y_continuous(breaks = 0.01*seq(-20, 20, .5),
-                     labels = percent_format(a = .1)) + 
-  theme(legend.position = "top",
-        legend.title = element_blank(),
-        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) + 
-  facet_wrap(~ variable)
+  theme(legend.position = c(0.4, 0.9),
+        legend.title = element_blank()) +
+  scale_y_continuous(breaks = 1000*seq(-100, 90, .5),
+                     labels = scales::dollar_format(acc = 1, su = " Md", pre = ""),
+                     limits = c(0, 9000))
 
 
-write_csv(figure5_fr, "figure5_fr.csv")
-ggsave("figure5_fr.png", width = 1.25*6, height = 1.25*3.375, bg = "white")
-ggsave("figure5_fr.pdf", width = 1.25*6, height = 1.25*3.375)
+write_csv(figure6_fr, "figure6_fr.csv")
+ggsave("figure6_fr.png", width = 1.25*6, height = 1.25*3.375, bg = "white")
+ggsave("figure6_fr.pdf", width = 1.25*6, height = 1.25*3.375)
 
