d078ca8d4b610bb7999778e37f38ecc1a2c39106
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2025-07-25 09:41:02 +0200

    update

--- _build.R
+++ _build.R
@@ -1,11 +1,11 @@
 # build.R
 
 # Define the range of figures to include
-figure_numbers <- 3:8
+figure_numbers <- 2:8
 
 # Loop over each figure script and source it
 for (i in figure_numbers) {
   script_name <- sprintf("figure%d.R", i)
-  message("Running ", script_name)
+  message("▶️ Running", script_name)
   source(script_name)
 }
